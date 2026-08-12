759c316b6a6d51b4701b1c8640b9f4db7f57bf8f
naphatdev168 <napatch.dev@gmail.com>
2026-08-12 11:22:02 +0700

    chore: add code splitting to resolve Vercel build chunk size warning

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,18 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          excel: ['xlsx'],
+          pdf: ['jspdf']
+        }
+      }
+    }
+  },
   server: {
     port: 3000,
     host: true,
